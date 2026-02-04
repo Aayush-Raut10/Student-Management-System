fc8ef51afc399f01124a197ebd737f5031ecb689
Aayush-Raut10 <aayushraut209@gmail.com>
2026-02-04 22:00:42 +0545

    Initial user_register view

--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,29 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+
 
 # Create your views here.
 def user_register(request):
+
+    if request.method == "POST":
+
+        data = request.POST
+
+        errors = {}
+
+        username = str(data.get("username"))
+        password = str(data.get("password"))
+        confirm_password = str(data.get("confirm_password"))
+
+        if username is None and username == " ":
+            errors["username"] = "username required"
+
+        if password != confirm_password:
+            errors["password"] = "Password do not match"
+      
+        if errors:
+            redirect("register")
+
+        else:
+            redirect("login")
+    
     return render(request, "accounts/register.html")

0d11f3917e0434acb03ba6402f1f378f1a6749ec
Aayush-Raut10 <aayushraut209@gmail.com>
2026-02-04 23:25:50 +0545

    storing user info

--- accounts/views.py
+++ accounts/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
 
 
 # Create your views here.
@@ -10,21 +11,29 @@ def user_register(request):
 
         errors = {}
 
-        username = str(data.get("username"))
+        username = str(data.get("username")).strip()
         password = str(data.get("password"))
         confirm_password = str(data.get("confirm_password"))
 
-        if username is None and username == " ":
+        if not username:
             errors["username"] = "username required"
+
+        elif User.objects.filter(username=username).exists():
+            errors["username"] = "Username already exists"
 
         if password != confirm_password:
             errors["password"] = "Password do not match"
-      
+
         if errors:
-            redirect("register")
+            print(errors)
+            return render(request, "accounts/register.html", context={
+                'errors':errors.values(),
+            })
 
         else:
-            redirect("login")
+
+            User.objects.create_user(username=username, password=password)
+            return redirect("login")
     
     return render(request, "accounts/register.html")
 
